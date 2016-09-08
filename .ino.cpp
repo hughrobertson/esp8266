b65c5b827c1dc39f74dcaff4ab179d666d437f8f
hugh <hughrobertson44@gmail.com>
2016-09-08 11:02:42 +0100

    initial project version .* files added

---
+++ .ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-09-08 10:58:28
+
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+void setGPIO(int chan) ;
+void setup() ;
+void controlON(int chan) ;
+void controlOFF(int chan) ;
+void loop() ;
+
+#include "relayDriver.ino"
+
+
+#endif
